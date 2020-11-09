c5bd42c35a1da49d70517abf8307204cb373fda8
prometey47 <74191926+prometey47@users.noreply.github.com>
2020-11-10 03:38:34 +0900

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa93ecebadc03001fb0e104",
+				"5fa93ecebadc03001fb0e106"
+			]
+		},
+		"5fa93ecebadc03001fb0e104": {
+			"id": "5fa93ecebadc03001fb0e104",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa93ecebadc03001fb0e106": {
+			"id": "5fa93ecebadc03001fb0e106",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
